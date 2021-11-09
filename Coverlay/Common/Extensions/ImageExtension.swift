1893453d6d4f5db07be7d772a476356d6a29be88
Vadim Krasilnikov <vkrasilnikov@diasoft.ru>
2021-11-09 17:44:30 +0300

    Added tap gesture on image view that allows to flip image horizontally.

--- Coverlay/Common/Extensions/ImageExtension.swift
+++ Coverlay/Common/Extensions/ImageExtension.swift
@@ -10,6 +10,10 @@ import UIKit
 //MARK: - UIImage
 extension UIImage {
     
+    enum Axis {
+        case horizontally, vertically
+    }
+    
     func withAlphaComponent(_ alpha: CGFloat) -> UIImage? {
         UIGraphicsBeginImageContextWithOptions(size, false, scale)
         defer { UIGraphicsEndImageContext() }
@@ -30,14 +34,37 @@ extension UIImage {
         UIGraphicsEndImageContext()
         return coloredImage
     }
+    
+    func flipped(_ axis: Axis) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        
+        let context = UIGraphicsGetCurrentContext()!
+        context.translateBy(x: self.size.width/2, y: self.size.height/2)
+        
+        switch axis {
+        case .horizontally:
+            context.scaleBy(x: -1.0, y: 1.0)
+            
+        case .vertically:
+            context.scaleBy(x: 1.0, y: -1.0)
+        }
+        
+        context.translateBy(x: -self.size.width/2, y: -self.size.height/2)
+        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
 }
 
 //MARK: - UIImageView
 extension UIImageView {
-    
     func becomeGestureTransformable(isDraggable: Bool = true,
                                     isRotatable: Bool = true,
                                     isResizable: Bool = true,
+                                    isFlippable: Bool = true,
                                     gestureRecognizerDelegate: UIGestureRecognizerDelegate) {
         if isDraggable {
             let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(dragImageView(sender:)))
@@ -55,6 +82,11 @@ extension UIImageView {
             let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(resizeImageView(sender:)))
             pinchGestureRecognizer.delegate = gestureRecognizerDelegate
             self.addGestureRecognizer(pinchGestureRecognizer)
+        }
+        
+        if isFlippable {
+            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(flipImageHorizontally(sender:)))
+            self.addGestureRecognizer(tapGestureRecognizer)
         }
     }
     
@@ -85,5 +117,11 @@ extension UIImageView {
     private func resizeImageView(sender: UIPinchGestureRecognizer) {
         self.transform = self.transform.scaledBy(x: sender.scale, y: sender.scale)
         sender.scale = 1.0
+    }
+    
+    //MARK: - Flip action
+    @objc
+    private func flipImageHorizontally(sender: UITapGestureRecognizer) {
+        self.image = image?.flipped(.horizontally)
     }
 }
